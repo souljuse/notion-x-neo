7cd85c94227479e3d4058df137de1f40ab2149ef
Irene Oppo <i.oppo@datocms.com>
2021-05-06 16:32:54 +0200

    First commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
+  env: {
+    rootNotionPageId: process.env.NOTION_PAGE_ID
+  },
+  images: {
+    domains: ['pbs.twimg.com']
+  },
+  future: {
+    webpack5: true
+  }
+})
